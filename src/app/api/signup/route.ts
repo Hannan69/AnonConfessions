103aa22d07f5b836421fd8ed6d64b520d9d0a34e
Hannan69 <hannanppp@gmail.com>
2025-03-27 22:53:57 +0500

    Signup half Done

--- src/app/api/signup/route.ts
+++ src/app/api/signup/route.ts
@@ -1,1 +1,39 @@
 import dbConnect from "@/lib/dbConnect";
+import { NextRequest,NextResponse } from "next/server";
+import UserModel from "@/models/User";
+import bcrypt from "bcryptjs"
+import { sendVerificationEmail } from "../send/route";
+
+
+export async function POST (request: NextRequest){
+    await dbConnect()
+    try {
+        const {username,email,password} = await request.json()
+        //will return user who are verified
+        const userExistsUsername = await UserModel.findOne({
+            username,
+            isVerified: true
+        })
+        if(userExistsUsername){
+            return Response.json({
+                success: false,
+                message: 'Username is already taken'
+            },{
+                status:400
+            })
+        }
+        const checkEmail = await UserModel.findOne({email})
+
+        if(checkEmail){
+            
+        }
+    } catch (error) {
+        console.log("Error signing up", error)
+        return Response.json({
+            success: false,
+            message: 'Error while registering'
+        },{
+            status:500
+        })
+    }
+}
